c857de1ff010a70af6fd6271bfb9184404874c51
João Lucas <lucasholanda11@hotmail.com>
2022-12-21 10:31:55 -0300

    update in js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const button = document.querySelector('button')
+//endereco dos campos!
+    const button = document.querySelector('button')
+   
 
 const addloading = () =>{
     button.innerHTML = '<img src="./assets/loading.png" class="loading">'
@@ -6,14 +8,17 @@ const addloading = () =>{
 
 const removeloading = () =>{
     button.innerHTML = 'Enviar'
+    
 }
 
+const completesend = () => {
+    window.location.href('https://www.instagram.com/com.sagradoamor/', 'blank')
+}
 
 //tratando cada campo!
 const handlesubmit = (event ) =>{
-    event.preventDefault();
+    event.preventDefault(); 
     addloading()
-    //querys
     const nome = document.querySelector('input[name=nome]').value;
     const sobrenome = document.querySelector('input[name=sobrenome]').value;
     const idade = document.querySelector('#idade').value
@@ -22,18 +27,23 @@ const handlesubmit = (event ) =>{
     const telefone = document.querySelector('input[name=telefone]').value
     const endereco = document.querySelector('input[name=end]').value
     const intencoes = document.querySelector('textarea[name=intencoes]').value
+    const DiaDoPagamento = document.querySelector('#payday').value
+    const group = document.querySelector('#grupoinfo').value
+    const metodoPagamento = document.querySelector('#methodpay').value
 
-
-    fetch('https://api.sheetmonkey.io/form/jNfP6bTXqi3RjbUV4JJ3M3',{
+    fetch('https://api.sheetmonkey.io/form/gTYcXgzN75Dc9fYzJhJh2y',{
 
     method:'post',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify({nome,sobrenome,idade,nascimento,email,telefone,endereco,intencoes}),
+    body: JSON.stringify({nome,sobrenome,idade,nascimento,email,telefone,endereco,intencoes,group,DiaDoPagamento,metodoPagamento}),
 
     }).then(() => removeloading())
+
+    
+
 }
 
 
